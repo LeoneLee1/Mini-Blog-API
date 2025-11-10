cd7debaa1e69626121bd21c7a26c7c239fe79e39
laravel-webprofil <mawoloti@gmail.com>
2025-11-11 02:02:41 +0700

    database created, and seeding database

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::create([
+        //     'name' => 'Daniel Lee Runtuwene',
+        //     'email' => 'danielruntuwene42@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make(123456),
+        //     'remember_token' => Str::random(10),
+        // ]);
+    }
+}
